b87fb315b0ccd0a0c61a66087d75cb5db0ffa96c
Amos Jun-yeung Ng <me@amos.ng>
2023-08-16 05:10:20 +0000

    Different changes

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,23 +1,11 @@
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+    format!("Hello, {}!", name)
 }
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::greet;
-
-    #[test]
-    fn test_greet_name() {
-        let result = greet("Test");
-        assert_eq!(result, "Hello, Test! You've been greeted from Rust!");
-    }
+        .expect("unable to run Tauri application");
 }
