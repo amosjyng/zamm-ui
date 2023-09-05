b8882929525364ce647b4c75a03356f038812996
Amos Jun-yeung Ng <me@amos.ng>
2023-09-05 01:20:41 +0000

    Rename Header to Sidebar in preparation for making it a sidebar

--- src-svelte/src/routes/+layout.svelte
+++ src-svelte/src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Header from "./Header.svelte";
+  import Sidebar from "./Sidebar.svelte";
   import "./styles.css";
 </script>
 
 <div class="app">
-  <Header />
+  <Sidebar />
 
   <main>
     <slot />
