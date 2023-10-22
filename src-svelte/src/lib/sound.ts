6c04f565e7edc1961664e8cc118fea5084556ede
Amos Jun-yeung Ng <me@amos.ng>
2023-10-22 01:06:34 +0000

    Don't stop execution just because sound can't be played

--- src-svelte/src/lib/sound.ts
+++ src-svelte/src/lib/sound.ts
@@ -5,7 +5,11 @@ import { soundOn, volume } from "./preferences";
 export function playSoundEffect(sound: Sound) {
   if (get(soundOn)) {
     const soundEffectVolume = get(volume);
-    playSound(sound, soundEffectVolume);
+    try {
+      playSound(sound, soundEffectVolume);
+    } catch (e) {
+      console.error(`Problem playing ${sound}: ${e}`);
+    }
     if (window._testRecordSoundPlayed !== undefined) {
       window._testRecordSoundPlayed();
     }
