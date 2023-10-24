491dea62907bb73906bd82bedfed3c2856ba31cb
Amos Jun-yeung Ng <me@amos.ng>
2023-10-24 23:32:09 +0000

    Boost volume when animation speed low

--- src-svelte/src/lib/sound.ts
+++ src-svelte/src/lib/sound.ts
@@ -4,8 +4,9 @@ import { soundOn, volume } from "./preferences";
 
 export function playSoundEffect(sound: Sound, speed?: number) {
   if (get(soundOn)) {
-    const soundEffectVolume = get(volume);
     const soundEffectSpeed = speed || 1;
+    // boost volume to compensate for lowered volume from lower speed
+    const soundEffectVolume = get(volume) / soundEffectSpeed;
     try {
       playSound(sound, soundEffectVolume, soundEffectSpeed);
     } catch (e) {
