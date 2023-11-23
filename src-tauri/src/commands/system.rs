1b760a2758899f0debafe7168727789764e4e98c
Amos Jun-yeung Ng <me@amos.ng>
2023-11-23 06:52:29 +0000

    Test exact API call

--- src-tauri/src/commands/system.rs
+++ src-tauri/src/commands/system.rs
@@ -52,6 +52,26 @@ pub fn get_system_info() -> SystemInfo {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::sample_call::SampleCall;
+    use std::fs;
+
+    fn parse_system_info(response_str: &str) -> SystemInfo {
+        serde_json::from_str(response_str).unwrap()
+    }
+
+    fn read_sample(filename: &str) -> SampleCall {
+        let sample_str = fs::read_to_string(filename)
+            .unwrap_or_else(|_| panic!("No file found at {filename}"));
+        serde_yaml::from_str(&sample_str).unwrap()
+    }
+
+    fn check_get_system_info_sample(file_prefix: &str, actual_info: &SystemInfo) {
+        let system_info_sample = read_sample(file_prefix);
+        assert_eq!(system_info_sample.request, vec!["get_system_info"]);
+
+        let expected_info = parse_system_info(&system_info_sample.response);
+        assert_eq!(actual_info, &expected_info);
+    }
 
     #[test]
     fn test_can_determine_shell() {
@@ -66,5 +86,18 @@ mod tests {
         let shell_init_file = get_shell_init_file(&shell);
         println!("Shell init file is {}", shell_init_file);
         assert!(shell_init_file.ends_with("/.zshrc"));
+    }
+
+    #[test]
+    fn test_get_linux_system_info() {
+        let system_info = SystemInfo {
+            shell: Some(Shell::Zsh),
+            shell_init_file: Some("/root/.zshrc".to_string()),
+        };
+
+        check_get_system_info_sample(
+            "./api/sample-calls/get_system_info-linux.yaml",
+            &system_info,
+        );
     }
 }
