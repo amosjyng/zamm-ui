b682aeac7af6ec5c1a68b793745e2c64de9658a7
Amos Jun-yeung Ng <me@amos.ng>
2023-12-13 11:07:37 +0000

    Add snackbar, without tests or proper Storybook stories

---
+++ src-svelte/src/lib/Snackbar.svelte
@@ -0,0 +1,87 @@
+<script lang="ts" context="module">
+  import { writable, type Writable } from "svelte/store";
+  import { fly, fade } from "svelte/transition";
+  import { flip } from "svelte/animate";
+  import IconClose from "~icons/ep/close-bold";
+
+  interface SnackbarMessage {
+    id: number;
+    msg: string;
+  }
+
+  export const snackbars: Writable<SnackbarMessage[]> = writable([]);
+  export let durationMs = 5_000;
+  let animateDurationMs = 1_000;
+
+  let nextId = 0;
+
+  // Function to show a new snackbar message
+  export function snackbarError(msg: string) {
+    animateDurationMs = 1_000;
+    const id = nextId++;
+    snackbars.update((current) => [...current, { id, msg }]);
+
+    // Auto-dismiss after 'duration'
+    setTimeout(() => {
+      dismiss(id);
+    }, durationMs);
+  }
+
+  // Function to manually dismiss a snackbar
+  function dismiss(id: number) {
+    animateDurationMs = 1_000 * 2;
+    snackbars.update((current) =>
+      current.filter((snackbar) => snackbar.id !== id),
+    );
+  }
+</script>
+
+<div class="snackbars">
+  {#each $snackbars as snackbar (snackbar.id)}
+    <div
+      class="snackbar"
+      in:fly|global={{ y: "1rem", duration: 1000 }}
+      out:fade|global={{ duration: 1000 }}
+      animate:flip={{ duration: animateDurationMs }}
+    >
+      {snackbar.msg}
+      <button on:click={() => dismiss(snackbar.id)}>
+        <IconClose />
+      </button>
+    </div>
+  {/each}
+</div>
+
+<style>
+  .snackbars {
+    width: 100%;
+    position: fixed;
+    bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+  }
+
+  .snackbar {
+    padding: 0.5rem 1rem;
+    display: flex;
+    justify-content: space-between;
+    gap: 1rem;
+    background-color: var(--color-error);
+    color: white;
+    border-radius: 4px;
+    filter: drop-shadow(0px 1px 4px #cc0000);
+    width: fit-content;
+    margin: 0 auto;
+  }
+
+  button {
+    background: none;
+    border: none;
+    color: white;
+    cursor: pointer;
+    padding: 0.5rem;
+    margin: -0.5rem;
+    align-self: flex-end;
+  }
+</style>
