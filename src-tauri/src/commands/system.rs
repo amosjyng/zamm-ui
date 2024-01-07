28b21e117c60428532b842569b75d4509b99f797
Amos Jun-yeung Ng <me@amos.ng>
2024-01-07 14:45:43 +1100

    Only use profile init files as fallback

--- src-tauri/src/commands/system.rs
+++ src-tauri/src/commands/system.rs
@@ -50,7 +50,7 @@ fn get_shell() -> Option<Shell> {
     None
 }
 
-fn get_relative_shell_init_file() -> Option<String> {
+fn get_relative_profile_init_file() -> Option<String> {
     #[cfg(target_os = "linux")]
     return Some("~/.profile".to_string());
     #[cfg(target_os = "macos")]
@@ -59,18 +59,28 @@ fn get_relative_shell_init_file() -> Option<String> {
     return None;
 }
 
-fn get_shell_init_file() -> Option<String> {
-    get_relative_shell_init_file().map(|f| shellexpand::tilde(&f).to_string())
+fn get_shell_init_file(shell: &Option<Shell>) -> Option<String> {
+    let relative_file = match shell {
+        Some(Shell::Bash) => Some("~/.bashrc".to_string()),
+        Some(Shell::Zsh) => Some("~/.zshrc".to_string()),
+        None => get_relative_profile_init_file(),
+    };
+    relative_file
+        .as_ref()
+        .map(|f| shellexpand::tilde(f).to_string())
 }
 
 #[tauri::command(async)]
 #[specta]
 pub fn get_system_info() -> SystemInfo {
+    let shell = get_shell();
+    let shell_init_file = get_shell_init_file(&shell);
+
     SystemInfo {
         zamm_version: get_zamm_version(),
         os: get_os(),
-        shell: get_shell(),
-        shell_init_file: get_shell_init_file(),
+        shell,
+        shell_init_file,
     }
 }
 
@@ -126,10 +136,21 @@ mod tests {
 
     #[test]
     fn test_can_predict_shell_init() {
-        let shell_init_file = get_shell_init_file().unwrap();
+        let shell = Shell::Zsh;
+        let shell_init_file = get_shell_init_file(&Some(shell));
+        println!("Shell init file is {:?}", shell_init_file);
+        assert!(shell_init_file.is_some());
+        let file_path = shell_init_file.unwrap();
+        assert!(file_path.starts_with('/'));
+        assert!(file_path.ends_with(".zshrc"));
+    }
+
+    #[test]
+    fn test_can_predict_profile_init() {
+        let shell_init_file = get_shell_init_file(&None).unwrap();
         println!("Shell init file is {}", shell_init_file);
         assert!(shell_init_file.starts_with('/'));
-        assert!(shell_init_file.ends_with("profile"));
+        assert!(shell_init_file.ends_with(".profile"));
     }
 
     #[test]
@@ -138,7 +159,7 @@ mod tests {
             zamm_version: "0.0.0".to_string(),
             os: Some(OS::Linux),
             shell: Some(Shell::Zsh),
-            shell_init_file: Some("/root/.profile".to_string()),
+            shell_init_file: Some("/root/.zshrc".to_string()),
         };
 
         check_get_system_info_sample(
