0b725ecc784b36fa93a1b70e66c31caebc80d702
Amos Jun-yeung Ng <me@amos.ng>
2024-01-19 19:26:15 +1100

    Refactor out round svg definition

--- src-svelte/src/lib/InfoBox.svelte
+++ src-svelte/src/lib/InfoBox.svelte
@@ -242,6 +242,7 @@
 
 <script lang="ts">
   import getComponentId from "./label-id";
+  import RoundDef from "./RoundDef.svelte";
   import { cubicInOut, cubicOut, linear } from "svelte/easing";
   import { animationSpeed, animationsOn } from "./preferences";
   import { fade, type TransitionConfig } from "svelte/transition";
@@ -495,26 +496,7 @@
   style={maxWidthStyle}
   in:fade|global={overallFadeInArgs}
 >
-  <svg
-    style="visibility: hidden; position: absolute;"
-    width="0"
-    height="0"
-    xmlns="http://www.w3.org/2000/svg"
-    version="1.1"
-  >
-    <defs>
-      <filter id="round">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
-          result="goo"
-        />
-        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-      </filter>
-    </defs>
-  </svg>
+  <RoundDef />
 
   <div class="border-container">
     <div class="border-box" in:revealOutline|global={timing.borderBox}></div>
