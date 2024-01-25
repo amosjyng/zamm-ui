7b6d1729d01f7107bed76cd0e44292cbac2c9b33
Amos Jun-yeung Ng <me@amos.ng>
2024-01-25 16:08:49 +1100

    Style chat field differently

--- src-svelte/src/lib/controls/Button.svelte
+++ src-svelte/src/lib/controls/Button.svelte
@@ -1,50 +1,34 @@
 <script lang="ts">
   export let text: string;
+  export let unwrapped = false;
+  export let rightEnd = false;
 </script>
 
-<button class="outer" type="submit">
-  <div class="inner">{text}</div>
-</button>
+{#if unwrapped}
+  <button class="cut-corners inner" class:right-end={rightEnd} type="submit">
+    {text}
+  </button>
+{:else}
+  <button class="cut-corners outer" class:right-end={rightEnd} type="submit">
+    <div class="cut-corners inner" class:right-end={rightEnd}>{text}</div>
+  </button>
+{/if}
 
 <style>
   .outer,
   .inner {
     --cut: var(--controls-corner-cut);
-    --background-color: var(--color-background);
-    --border-color: #ccc;
-    --border: 0.15rem;
-    --diagonal-border: calc(var(--border) * 0.8);
     font-size: 0.9rem;
     font-family: var(--font-body);
-    border: var(--border) solid var(--border-color);
     text-transform: uppercase;
-    background:
-      linear-gradient(
-          -45deg,
-          var(--border-color) 0 calc(var(--cut) + var(--diagonal-border)),
-          var(--background-color) 0
-        )
-        bottom right / 50% 100%,
-      linear-gradient(
-          135deg,
-          var(--border-color) 0 calc(var(--cut) + var(--diagonal-border)),
-          var(--background-color) 0
-        )
-        top left / 50% 100%;
-    background-origin: border-box;
-    background-repeat: no-repeat;
-    -webkit-mask:
-      linear-gradient(-45deg, transparent 0 var(--cut), #fff 0) bottom right,
-      linear-gradient(135deg, transparent 0 var(--cut), #fff 0) top left;
-    -webkit-mask-size: 51% 100%;
-    -webkit-mask-repeat: no-repeat;
-    mask:
-      linear-gradient(-45deg, transparent 0 var(--cut), #fff 0) bottom right,
-      linear-gradient(135deg, transparent 0 var(--cut), #fff 0) top left;
-    mask-size: 51% 100%;
-    mask-repeat: no-repeat;
     transition-property: filter, transform;
     transition: calc(0.5 * var(--standard-duration)) ease-out;
+  }
+
+  .outer.right-end,
+  .inner.right-end {
+    --cut-top-left: 0.01rem;
+    height: 100%;
   }
 
   .inner {
@@ -61,10 +45,6 @@
 
   .outer {
     --background-color: #eee;
-    --border: 2px;
-    --diagonal-border: 2.5px;
-    --cut: 8px;
-    padding: 1px;
     display: inline-block;
   }
 </style>
--- src-svelte/src/routes/chat/Chat.svelte
+++ src-svelte/src/routes/chat/Chat.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
   import InfoBox from "$lib/InfoBox.svelte";
-  import TextInput from "$lib/controls/TextInput.svelte";
-  import Button from "$lib/controls/Button.svelte";
   import Message from "./Message.svelte";
   import { type ChatMessage, chat } from "$lib/bindings";
   import { snackbarError } from "$lib/snackbar/Snackbar.svelte";
+  import Form from "./Form.svelte";
   import { onMount } from "svelte";
 
-  let currentMessage: string;
   export let conversation: ChatMessage[] = [
     {
       role: "System",
@@ -44,24 +42,20 @@
     }
   }
 
-  async function sendChat() {
-    const message = currentMessage.trim();
-    if (message) {
-      const chatMessage: ChatMessage = {
-        role: "Human",
-        text: message,
-      };
-      conversation = [...conversation, chatMessage];
-      currentMessage = "";
-      setTimeout(showChatBottom, 50);
+  async function sendChatMessage(message: string) {
+    const chatMessage: ChatMessage = {
+      role: "Human",
+      text: message,
+    };
+    conversation = [...conversation, chatMessage];
+    setTimeout(showChatBottom, 50);
 
-      try {
-        let llmCall = await chat("OpenAI", "gpt-3.5-turbo", null, conversation);
-        conversation = [...conversation, llmCall.response.completion];
-        setTimeout(showChatBottom, 50);
-      } catch (err) {
-        snackbarError(err as string);
-      }
+    try {
+      let llmCall = await chat("OpenAI", "gpt-3.5-turbo", null, conversation);
+      conversation = [...conversation, llmCall.response.completion];
+      setTimeout(showChatBottom, 50);
+    } catch (err) {
+      snackbarError(err as string);
     }
   }
 </script>
@@ -83,15 +77,7 @@
       </div>
     </div>
 
-    <form autocomplete="off" on:submit|preventDefault={sendChat}>
-      <label for="message" class="accessibility-only">Chat with the AI:</label>
-      <TextInput
-        name="message"
-        placeholder="Type your message here..."
-        bind:value={currentMessage}
-      />
-      <Button text="Send" />
-    </form>
+    <Form {sendChatMessage} />
   </div>
 </InfoBox>
 
@@ -117,13 +103,5 @@
     font-size: 0.85rem;
     font-style: italic;
     text-align: center;
-  }
-
-  form {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between;
-    gap: 1rem;
   }
 </style>
