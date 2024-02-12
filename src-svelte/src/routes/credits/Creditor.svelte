11a54586b0bb84941211164054d00caa02cb11fb
Amos Jun-yeung Ng <me@amos.ng>
2024-02-12 13:45:31 +1100

    Extract GitHub username

--- src-svelte/src/routes/credits/Creditor.svelte
+++ src-svelte/src/routes/credits/Creditor.svelte
@@ -1,9 +1,8 @@
-<script lang="ts">
-  export let name: string;
-  export let url: string;
-  export let urlDisplay = formatUrl(url);
-
-  function formatUrl(url: string) {
+<script lang="ts" context="module">
+  export function formatUrl(url: string) {
+    if (url.startsWith("https://github.com/")) {
+      return url.slice(19, -1);
+    }
     if (url.startsWith("https://")) {
       return url.slice(8);
     }
@@ -12,6 +11,12 @@
     }
     return url;
   }
+</script>
+
+<script lang="ts">
+  export let name: string;
+  export let url: string;
+  export let urlDisplay = formatUrl(url);
 </script>
 
 <div class="creditor">
---
+++ src-svelte/src/routes/credits/Creditor.test.ts
@@ -0,0 +1,13 @@
+import { expect, test } from "vitest";
+import { formatUrl } from "./Creditor.svelte";
+
+describe("URL formatter", () => {
+  test("formats HTTP(S) URL correctly", () => {
+    expect(formatUrl("http://yahoo.com")).toEqual("yahoo.com");
+    expect(formatUrl("https://google.com")).toEqual("google.com");
+  });
+
+  test("formats Github URLs correctly", () => {
+    expect(formatUrl("https://github.com/amosjyng/")).toEqual("amosjyng");
+  });
+});
