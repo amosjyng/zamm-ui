5495a45caaf1358ad0864f5af14c9e108bf5ffb7
Amos Jun-yeung Ng <me@amos.ng>
2024-02-14 22:04:48 +1100

    Successfully stop scroll on mouse events

--- src-svelte/src/routes/credits/Credits.svelte
+++ src-svelte/src/routes/credits/Credits.svelte
@@ -19,7 +19,6 @@
     const mainContainer = document.getElementById("main-container");
     if (mainContainer) {
       scrollElement = mainContainer as HTMLDivElement;
-      document.addEventListener("DOMMouseScroll", stopScrolling, false);
       setTimeout(() => {
         scrollInterval = setInterval(function () {
           if (scrollElement) {
@@ -31,105 +30,109 @@
 
     return function () {
       stopScrolling();
-      document.removeEventListener("DOMMouseScroll", stopScrolling, false);
     };
   });
 </script>
 
-<InfoBox title="Credits">
-  <div class="container">
-    <SubInfoBox
-      subheading="Zen and the Automation of Metaprogramming for the Masses"
-    >
-      <Creditor name="Amos Jun-yeung Ng" url="https://github.com/amosjyng/" />
-    </SubInfoBox>
-  </div>
-  <div class="container">
-    <SubInfoBox subheading="Frameworks">
-      <Grid>
-        <Creditor name="Tauri" logo="tauri.png" url="https://tauri.app/" />
-        <Creditor name="Svelte" logo="svelte.svg" url="https://svelte.dev/" />
-      </Grid>
-    </SubInfoBox>
-    <SubInfoBox subheading="Frontend">
-      <Grid>
-        <Creditor
-          name="Neodrag"
-          logo="neodrag.svg"
-          url="https://www.neodrag.dev/"
-        />
-        <Creditor name="Autosize" url="https://www.jacklmoore.com/autosize/" />
-        <Creditor name="Nano ID" url="https://github.com/ai/nanoid" />
-        <Creditor
-          name="Storybook"
-          logo="storybook.svg"
-          url="https://storybook.js.org/"
-        />
-        <Creditor name="Vite" logo="vite.svg" url="https://vitejs.dev/" />
-        <Creditor name="Vite" logo="vitest.svg" url="https://vitest.dev/" />
-        <Creditor
-          name="Playwright"
-          logo="playwright.svg"
-          url="https://playwright.dev/"
-        />
-        <Creditor
-          name="WebdriverIO"
-          logo="webdriverio.svg"
-          url="https://webdriver.io/"
-        />
-        <Creditor
-          name="Jest-Image-Snapshot"
-          logo="jest-image-snapshot.png"
-          url="https://github.com/americanexpress/jest-image-snapshot"
-        />
-      </Grid>
-    </SubInfoBox>
-    <SubInfoBox subheading="Backend">
-      <Grid>
-        <Creditor name="Serde" url="https://serde.rs/" />
-        <Creditor name="Diesel" logo="diesel.png" url="https://diesel.rs/" />
-        <Creditor
-          name="async-openai"
-          logo="async-openai.png"
-          url="https://github.com/64bit/async-openai"
-        />
-        <Creditor
-          name="Specta"
-          logo="specta.png"
-          url="https://github.com/oscartbeaumont/specta"
-        />
-        <Creditor name="rvcr" url="https://github.com/ChorusOne/rvcr" />
-        <Creditor
-          name="reqwest-middleware"
-          url="https://github.com/TrueLayer/reqwest-middleware"
-        />
-      </Grid>
-    </SubInfoBox>
-    <SubInfoBox subheading="General Tooling">
-      <Grid>
-        <Creditor
-          name="quicktype"
-          logo="quicktype.png"
-          url="https://quicktype.io/"
-        />
-        <Creditor
-          name="Docker"
-          logo="docker.svg"
-          url="https://www.docker.com/"
-        />
-      </Grid>
-    </SubInfoBox>
-  </div>
-</InfoBox>
-
-<div class="dedication-container">
-  <InfoBox title="Dedication" childNumber={1}>
-    <p class="atomic-reveal">
-      This project is made with ❤️, and dedicated to <strong
-        >Eliza Lynn Kith</strong
-      >.
-    </p>
+<div on:mousedown={stopScrolling} on:wheel={stopScrolling} role="none">
+  <InfoBox title="Credits">
+    <div class="container">
+      <SubInfoBox
+        subheading="Zen and the Automation of Metaprogramming for the Masses"
+      >
+        <Creditor name="Amos Jun-yeung Ng" url="https://github.com/amosjyng/" />
+      </SubInfoBox>
+    </div>
+    <div class="container">
+      <SubInfoBox subheading="Frameworks">
+        <Grid>
+          <Creditor name="Tauri" logo="tauri.png" url="https://tauri.app/" />
+          <Creditor name="Svelte" logo="svelte.svg" url="https://svelte.dev/" />
+        </Grid>
+      </SubInfoBox>
+      <SubInfoBox subheading="Frontend">
+        <Grid>
+          <Creditor
+            name="Neodrag"
+            logo="neodrag.svg"
+            url="https://www.neodrag.dev/"
+          />
+          <Creditor
+            name="Autosize"
+            url="https://www.jacklmoore.com/autosize/"
+          />
+          <Creditor name="Nano ID" url="https://github.com/ai/nanoid" />
+          <Creditor
+            name="Storybook"
+            logo="storybook.svg"
+            url="https://storybook.js.org/"
+          />
+          <Creditor name="Vite" logo="vite.svg" url="https://vitejs.dev/" />
+          <Creditor name="Vite" logo="vitest.svg" url="https://vitest.dev/" />
+          <Creditor
+            name="Playwright"
+            logo="playwright.svg"
+            url="https://playwright.dev/"
+          />
+          <Creditor
+            name="WebdriverIO"
+            logo="webdriverio.svg"
+            url="https://webdriver.io/"
+          />
+          <Creditor
+            name="Jest-Image-Snapshot"
+            logo="jest-image-snapshot.png"
+            url="https://github.com/americanexpress/jest-image-snapshot"
+          />
+        </Grid>
+      </SubInfoBox>
+      <SubInfoBox subheading="Backend">
+        <Grid>
+          <Creditor name="Serde" url="https://serde.rs/" />
+          <Creditor name="Diesel" logo="diesel.png" url="https://diesel.rs/" />
+          <Creditor
+            name="async-openai"
+            logo="async-openai.png"
+            url="https://github.com/64bit/async-openai"
+          />
+          <Creditor
+            name="Specta"
+            logo="specta.png"
+            url="https://github.com/oscartbeaumont/specta"
+          />
+          <Creditor name="rvcr" url="https://github.com/ChorusOne/rvcr" />
+          <Creditor
+            name="reqwest-middleware"
+            url="https://github.com/TrueLayer/reqwest-middleware"
+          />
+        </Grid>
+      </SubInfoBox>
+      <SubInfoBox subheading="General Tooling">
+        <Grid>
+          <Creditor
+            name="quicktype"
+            logo="quicktype.png"
+            url="https://quicktype.io/"
+          />
+          <Creditor
+            name="Docker"
+            logo="docker.svg"
+            url="https://www.docker.com/"
+          />
+        </Grid>
+      </SubInfoBox>
+    </div>
   </InfoBox>
+
+  <div class="dedication-container">
+    <InfoBox title="Dedication" childNumber={1}>
+      <p class="atomic-reveal">
+        This project is made with ❤️, and dedicated to <strong
+          >Eliza Lynn Kith</strong
+        >.
+      </p>
+    </InfoBox>
+  </div>
 </div>
 
 <style>
